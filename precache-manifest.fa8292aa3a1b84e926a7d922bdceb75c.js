26ea9e1291acc0f6f5e21e57f8d0ed46c119a4fa
Cody Snyder <codymichaelsnyder@gmail.com>
2020-03-15 14:58:39 -0600

    Updates

---
+++ precache-manifest.fa8292aa3a1b84e926a7d922bdceb75c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ac378e843e850a124a7225fe825d701",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "63dff9c1c4679ca2034b",
+    "url": "/robofriends/static/css/main.979d6acd.chunk.css"
+  },
+  {
+    "revision": "d4dc956eb6df33e9687c",
+    "url": "/robofriends/static/js/2.3a7757fe.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.3a7757fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63dff9c1c4679ca2034b",
+    "url": "/robofriends/static/js/main.4548b567.chunk.js"
+  },
+  {
+    "revision": "6a5eb8d065c05feaff0d",
+    "url": "/robofriends/static/js/runtime-main.5530ba65.js"
+  }
+]);
